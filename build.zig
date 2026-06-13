a138d762ec2ab36f8394452c4d1f519468ea449d
zigfield-jr <236256456+zigfield-jr@users.noreply.github.com>
2026-06-13 22:15:53 +0200

    tar step

--- build.zig
+++ build.zig
@@ -52,7 +52,18 @@ pub fn build(b: *std.Build) void {
     if (std.mem.eql(u8, "", dest_ip)) {
         b.getInstallStep().dependOn(&install_artifact.step);
     } else {
-        const send_tar = SendTar.create(b, dest_ip, 10003);
+        const tar_exe = b.addExecutable(.{
+            .name = "tar",
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("build.zig"),
+                .target = b.graph.host,
+            }),
+        });
+
+        const send_tar = b.addRunArtifact(tar_exe);
+        send_tar.setName("send tarball to device");
+        send_tar.addFileArg(b.path("zig-out"));
+        send_tar.addArg(dest_ip);
         send_tar.step.dependOn(&install_artifact.step);
         b.getInstallStep().dependOn(&send_tar.step);
     }
@@ -86,72 +97,59 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&test_run_artifact.step);
 }
 
-const SendTar = struct {
-    pub const base_id: Step.Id = .custom;
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+    const arena = init.arena.allocator();
 
-    step: Step,
-    dest_ip: []const u8,
-    dest_port: u16,
-
-    pub fn create(owner: *std.Build, dest_ip: []const u8, dest_port: u16) *SendTar {
-        const send_tar = owner.allocator.create(SendTar) catch @panic("OOM");
-
-        send_tar.* = .{
-            .step = Step.init(.{
-                .id = base_id,
-                .name = owner.fmt("send tarball to {s}:{d}", .{ dest_ip, dest_port }),
-                .owner = owner,
-                .makeFn = make,
-            }),
-            .dest_ip = owner.dupe(dest_ip),
-            .dest_port = dest_port,
-        };
-
-        return send_tar;
+    const args = try init.minimal.args.toSlice(arena);
+    if (args.len != 3) {
+        fatal("wrong number of arguments {d}", .{args.len});
     }
 
-    fn make(step: *Step, options: Step.MakeOptions) !void {
-        _ = options; // No progress to report.
-        const b = step.owner;
-        const send_tar: *SendTar = @fieldParentPtr("step", step);
+    const install_path = args[1];
+    const dest_ip = args[2];
+    const dest_port: u16 = 10003;
 
-        var threaded: std.Io.Threaded = .init_single_threaded;
-        const io = threaded.io();
+    const ip_address = try std.Io.net.IpAddress.parse(dest_ip, dest_port);
+    var tcp_stream = try std.Io.net.IpAddress.connect(&ip_address, io, .{ .mode = .stream });
+    defer tcp_stream.close(io);
+    var tcp_buffer: [1024]u8 = undefined;
+    var tcp_writer = tcp_stream.writer(io, &tcp_buffer);
 
-        const ip_address = try std.Io.net.IpAddress.parse(send_tar.dest_ip, send_tar.dest_port);
-        var tcp_stream = try std.Io.net.IpAddress.connect(&ip_address, io, .{ .mode = .stream });
-        defer tcp_stream.close(io);
-        var tcp_buffer: [1024]u8 = undefined;
-        var tcp_writer = tcp_stream.writer(io, &tcp_buffer);
+    var gzip_buffer: [std.compress.flate.max_window_len]u8 = undefined;
+    var gzip_compress: std.compress.flate.Compress = try .init(&tcp_writer.interface, &gzip_buffer, .gzip, .default);
+    const gzip_writer = &gzip_compress.writer;
 
-        var gzip_buffer: [std.compress.flate.max_window_len]u8 = undefined;
-        var gzip_compress: std.compress.flate.Compress = try .init(&tcp_writer.interface, &gzip_buffer, .gzip, .default);
-        const gzip_writer = &gzip_compress.writer;
+    var tar_writer: std.tar.Writer = .{ .underlying_writer = gzip_writer };
 
-        var tar_writer: std.tar.Writer = .{ .underlying_writer = gzip_writer };
+    var install_dir = try std.Io.Dir.openDirAbsolute(io, install_path, .{ .iterate = true });
+    defer install_dir.close(io);
 
-        var install_dir = try std.Io.Dir.openDirAbsolute(io, b.getInstallPath(.prefix, ""), .{ .iterate = true });
-        defer install_dir.close(io);
+    var install_walker = try install_dir.walk(arena);
+    defer install_walker.deinit();
 
-        var install_walker = try install_dir.walk(b.allocator);
-        defer install_walker.deinit();
+    while (try install_walker.next(io)) |install_entry| {
+        switch (install_entry.kind) {
+            .file => {
+                const file = try install_entry.dir.openFile(io, install_entry.basename, .{ .mode = .read_only });
+                defer file.close(io);
+                var file_buffer: [1024]u8 = undefined;
+                var file_reader = file.reader(io, &file_buffer);
 
-        while (try install_walker.next(io)) |install_entry| {
-            switch (install_entry.kind) {
-                .file => {
-                    const file = try install_entry.dir.openFile(io, install_entry.basename, .{ .mode = .read_only });
-                    defer file.close(io);
-                    var file_buffer: [1024]u8 = undefined;
-                    var file_reader = file.reader(io, &file_buffer);
-
-                    try tar_writer.writeFile(install_entry.path, &file_reader, 0);
-                },
-                else => {},
-            }
+                try tar_writer.writeFile(install_entry.path, &file_reader, 0);
+            },
+            else => {},
         }
-
-        try tar_writer.finishPedantically();
-        try gzip_writer.flush();
-        try tcp_writer.interface.flush();
     }
-};
+
+    try tar_writer.finishPedantically();
+    try gzip_writer.flush();
+    try tcp_writer.interface.flush();
+
+    return std.process.cleanExit(io);
+}
+
+fn fatal(comptime format: []const u8, args: anytype) noreturn {
+    std.debug.print(format, args);
+    std.process.exit(1);
+}
